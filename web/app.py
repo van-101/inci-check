ca0428d844890b2f478796674245bd91073608a1
van_101 <rawatvanshita2002@gmail.com>
2023-01-15 03:37:09 +0530

    photos added

--- web/app.py
+++ web/app.py
@@ -5,5 +5,8 @@ app=Flask(__name__)
 @app.route('/')
 def main_page():
    return render_template('index.html')
+def work_page():
+   return render_template('scan.html')
+
 if __name__=='__main__':
     app.run(debug=True)
