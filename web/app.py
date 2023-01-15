53ac1c4439390bb42c5b77dc7c45641c351b452b
van_101 <rawatvanshita2002@gmail.com>
2023-01-15 05:57:28 +0530

    results page

--- web/app.py
+++ web/app.py
@@ -5,8 +5,11 @@ app=Flask(__name__)
 @app.route('/')
 def main_page():
    # return render_template('index.html')
-   return render_template('output.html')
-   # return render_template('output.html')
+   return render_template('result.html')
+
+# @app.route('/at_work')
+# def work_page():  
+#    return render_template('output.html')
 
 if __name__=='__main__':
     app.run(debug=True)
