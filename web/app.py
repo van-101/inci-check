b808c214a647dee0250e0675b72382d5e4239066
xtanion <anandshivam54321@gmail.com>
2023-01-15 06:37:49 +0530

    fixing routs

--- web/app.py
+++ web/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from text_recog import ocr_image
 app=Flask(__name__)
 
-@app.route('/')
-def main_page():
+@app.route('/upload')
+def upload():
+   if request.method == 'POST':
+       if request.files:
+           file = request.files["file"]
+           output = ocr_image(file)
+           print(output)
+           return render_template('index.html')
+   return render_template('output.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   if request.method == 'GET':
+        if request.form.get('tryus')== 'Try Us!':
+            return redirect(url_for('upload'))
    return render_template('index.html')
-   # return render_template('output.html')
 
 # @app.route('/at_work')
 # def work_page():  
